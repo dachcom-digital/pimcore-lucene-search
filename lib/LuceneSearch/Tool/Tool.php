06e71bc88ec252209409f1f0aa038362509f8753
Stefan Hagspiel <shagspiel@dachcom.ch>
2016-03-08 08:39:25 +0100

    remove deprecated sql method

--- lib/LuceneSearch/Tool/Tool.php
+++ lib/LuceneSearch/Tool/Tool.php
@@ -4,12 +4,4 @@ namespace LuceneSearch\Tool;
 
 class Tool {
 
-    public static function getCrawlerQuery() {
-
-        $queryFile = PIMCORE_PLUGINS_PATH . '/LuceneSearch/db/query.sql';
-
-        return file_get_contents( $queryFile );
-
-    }
-
 }
