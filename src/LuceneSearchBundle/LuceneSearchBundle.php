0fe3d1f1aba3a3a35bf6cc5c53bdabfc4c6ebc37
Stefan Hagspiel <shagspiel@dachcom.ch>
2018-01-09 12:30:10 +0100

    bump version

--- src/LuceneSearchBundle/LuceneSearchBundle.php
+++ src/LuceneSearchBundle/LuceneSearchBundle.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class LuceneSearchBundle extends AbstractPimcoreBundle
 {
-    const BUNDLE_VERSION = '2.0.3';
+    const BUNDLE_VERSION = '2.0.4';
 
     /**
      * @inheritDoc
